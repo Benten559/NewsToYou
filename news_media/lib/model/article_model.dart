461a6a65418acc65972087ded23b255be728bc0b
Ben <benten559@mail.fresnostate.edu>
2023-11-14 19:31:41 -0800

    hash as a property of the article model

--- news_media/lib/model/article_model.dart
+++ news_media/lib/model/article_model.dart
@@ -1,3 +1,5 @@
+import 'package:NewsToYou/utility/hash/hash_generator.dart';
+
 import 'source_model.dart';
 
 String nullCheck(String? apiValue) {
@@ -9,6 +11,7 @@ String nullCheck(String? apiValue) {
 
 class Article {
   final Map<String, dynamic> jsonData;
+  final String hash;
   final Source? source;
   final String? author;
   final String? title;
@@ -17,9 +20,11 @@ class Article {
   final String? urlToImage;
   final String? publishedAt;
   final String? content;
+  bool saved = false;
 
   Article({
     required this.jsonData,
+    required this.hash,
     this.source,
     this.author,
     this.title,
@@ -33,6 +38,7 @@ class Article {
   factory Article.fromJson(Map<String, dynamic> json) {
     return Article(
       jsonData: json, // Store the original JSON data here
+      hash: generateMD5Hash(json),
       source: Source.fromJson(json['source']),
       author: nullCheck(json['author']),
       title: nullCheck(json['title']),
@@ -44,57 +50,3 @@ class Article {
     );
   }
 }
-
-
-
-
-
-// import 'source_model.dart';
-
-// String nullCheck(String? apiValue){
-//   if (apiValue != null){
-//     return apiValue;
-//   }
-//   return "N/A";
-// }
-
-// class Article {
-//   // Map<String, dynamic> jsonData = '{"x":true}' as Map<String, dynamic>;
-//   Source? source;
-//   String? author = "0";
-//   String? title = "0";
-//   String? description = "0";
-//   String url = "0";
-//   String? urlToImage = "0";
-//   String? publishedAt = "0";
-//   String? content = "0";
- 
-//   Article(
-//       {this.source,
-//       this.author,
-//       this.title,
-//       this.description,
-//       required this.url,
-//       this.urlToImage,
-//       this.publishedAt,
-//       this.content,
-//       // required Map<String, dynamic> jsonData 
-//       });
-
-//   // Map toJson(){ return jsonData;}
-
-//   factory Article.fromJson(Map<String, dynamic> json) {
-//     return Article(
-//       // jsonData:json,
-//       source: Source.fromJson(json['source']),
-//       author: nullCheck(json['author']),
-//       title: nullCheck(json['title']),
-//       description: nullCheck(json['description']),
-//       url: nullCheck(json['url']),
-//       urlToImage: nullCheck(json['urlToImage']),
-//       publishedAt: nullCheck(json['publishedAt']),
-//       content: nullCheck(json['content']),
-//     );
-
-//   }
-// }
