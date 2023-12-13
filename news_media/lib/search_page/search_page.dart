007f8ca44c858974d1353ad391b950e3192450b8
Brian Bales <48467575+brianbales99@users.noreply.github.com>
2023-12-12 20:07:07 -0800

    Updated searchPage
    Removed back button

--- news_media/lib/search_page/search_page.dart
+++ news_media/lib/search_page/search_page.dart
@@ -23,6 +23,7 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text('Search Page'),
       ),
       body: Column(
